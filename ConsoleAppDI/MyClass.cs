4775e4deaeb980699873f3bb85d08170c918223c
blankakorvo <blankakorvo@gmail.com>
2020-10-08 10:33:16 +0300

    go

---
+++ ConsoleAppDI/MyClass.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleAppDI
+{
+    public class MyClass
+    {
+        private readonly ILogger<MyClass> _logger;
+        private readonly IConfiguration _configuration;
+
+        public MyClass(ILogger<MyClass> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public void DoSometing()
+        {
+            var val = _configuration.GetValue<string>("myParam");
+            _logger.LogInformation($"Called do something func with param {val}");
+        }
+    }
+}
---
+++ ConsoleAppDI/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace ConsoleAppDI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
+
+            var builder = new ConfigurationBuilder().AddJsonFile("mySettings.json");
+            IConfiguration configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+
+            serviceCollection.AddLogging(loggingBuilder => loggingBuilder.AddSerilog());
+
+            serviceCollection.AddSingleton<MyClass>();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var myClassFromDI = serviceProvider.GetRequiredService<MyClass>();
+
+            myClassFromDI.DoSometing();
+        }        
+    }
+}
